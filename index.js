046dfc82b68848f6bb94a61af33ef4b875453e05
kDot <k-laus@gmx.net>
2021-05-25 07:34:53 +0200

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-//appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log(appDiv.id);
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
